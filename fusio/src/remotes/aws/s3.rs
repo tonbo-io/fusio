e7099bc5246e5e8305ea46f85950128cdd8b2dab
Gwo Tzu-Hsing <gotzehsing@gmail.com>
2024-09-19 01:54:30 +0800

    feat: complete S3 reading test

--- fusio/src/remotes/aws/s3.rs
+++ fusio/src/remotes/aws/s3.rs
@@ -1,19 +1,114 @@
+use http::{header::RANGE, Method};
+use http_body_util::BodyExt;
 use hyper::Request;
 use percent_encoding::utf8_percent_encode;
 
-use super::STRICT_PATH_ENCODE_SET;
-use crate::{Error, IoBufMut, Read};
+use super::{
+    credential::{AwsAuthorizer, AwsCredential},
+    STRICT_PATH_ENCODE_SET,
+};
+use crate::{
+    path::Path,
+    remotes::http::{Empty, HttpClient},
+    Error, IoBuf, Read,
+};
 
-pub struct S3 {
+pub struct S3File<C: HttpClient> {
     bucket_endpoint: String,
-    path: String,
+    path: Path,
+    credential: AwsCredential,
+    region: String,
+    sign_payload: bool,
+
+    client: C,
 }
 
-impl S3 {
-    pub fn new(bucket_endpoint: String, path: String) -> Self {
-        Self {
-            bucket_endpoint,
-            path,
+impl<C: HttpClient> Read for S3File<C> {
+    async fn read(&mut self, pos: u64, len: Option<u64>) -> Result<impl IoBuf, Error> {
+        let url = format!(
+            "{}/{}",
+            self.bucket_endpoint,
+            utf8_percent_encode(self.path.as_ref(), &STRICT_PATH_ENCODE_SET)
+        );
+        let authorizer = AwsAuthorizer::new(&self.credential, "s3", &self.region)
+            .with_sign_payload(self.sign_payload);
+
+        let mut request = Request::builder()
+            .method(Method::GET)
+            .uri(url)
+            .header(
+                RANGE,
+                match len {
+                    Some(len) => format!("bytes={}-{}", pos, pos + len),
+                    None => format!("bytes={}-", pos),
+                },
+            )
+            .body(Empty {})?;
+
+        authorizer
+            .authorize(&mut request, None)
+            .await
+            .map_err(|e| Error::Other(e.into()))?;
+
+        let response = self
+            .client
+            .send_request(request)
+            .await
+            .map_err(Error::Other)?;
+
+        if response.status().is_success() {
+            Ok(response
+                .into_body()
+                .collect()
+                .await
+                .map_err(|e| Error::Other(e.into()))?
+                .to_bytes())
+        } else {
+            Err(Error::Other(
+                format!("failed to read from S3, HTTP status: {}", response.status()).into(),
+            ))
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[cfg(feature = "tokio-http")]
+    #[tokio::test]
+    async fn test_s3_file() {
+        use std::env;
+
+        use crate::{
+            remotes::{
+                aws::{credential::AwsCredential, s3::S3File},
+                http::tokio::TokioClient,
+            },
+            IoBuf, Read,
+        };
+
+        if env::var("AWS_ACCESS_KEY_ID").is_err() {
+            eprintln!("skipping AWS s3 test");
+            return;
+        }
+        let key_id = env::var("AWS_ACCESS_KEY_ID").unwrap();
+        let secret_key = env::var("AWS_SECRET_ACCESS_KEY").unwrap();
+
+        let client = TokioClient::new();
+        let region = "ap-southeast-1";
+        let mut s3 = S3File {
+            bucket_endpoint: "https://fusio-test.s3-ap-southeast-1.amazonaws.com".into(),
+            path: "test.txt".into(),
+            credential: AwsCredential {
+                key_id,
+                secret_key,
+                token: None,
+            },
+            region: region.into(),
+            sign_payload: true,
+            client,
+        };
+
+        let buf = s3.read(0, None).await.unwrap();
+        assert_eq!(buf.as_slice(), b"hello, world");
     }
 }
