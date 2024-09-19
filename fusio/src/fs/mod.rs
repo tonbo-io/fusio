2c4c8a6748f62c2c9d139bdeac4ca5f1ce583b8f
Gwo Tzu-Hsing <gotzehsing@gmail.com>
2024-09-19 12:12:58 +0800

    feat: restrict file as 'static

--- fusio/src/fs/mod.rs
+++ fusio/src/fs/mod.rs
@@ -6,38 +6,14 @@ use futures_core::Stream;
 pub use options::*;
 
 use crate::{path::Path, Error, Read, Write};
+use crate::{MaybeSend, MaybeSync};
 
 pub struct FileMeta {
     pub path: Path,
 }
 
-#[cfg(not(feature = "no-send"))]
-pub trait Fs: Send + Sync {
-    type File: Read + Write;
-
-    fn open(&self, path: &Path) -> impl Future<Output = Result<Self::File, Error>> + Send {
-        self.open_options(path, OpenOptions::default())
-    }
-
-    fn open_options(
-        &self,
-        path: &Path,
-        options: OpenOptions,
-    ) -> impl Future<Output = Result<Self::File, Error>> + Send;
-
-    fn create_dir(path: &Path) -> impl Future<Output = Result<(), Error>> + Send;
-
-    fn list(
-        &self,
-        path: &Path,
-    ) -> impl Future<Output = Result<impl Stream<Item = Result<FileMeta, Error>>, Error>> + Send;
-
-    fn remove(&self, path: &Path) -> impl Future<Output = Result<(), Error>> + Send;
-}
-
-#[cfg(feature = "no-send")]
-pub trait Fs {
-    type File: Read + Write;
+pub trait Fs: MaybeSend + MaybeSync {
+    type File: Read + Write + 'static;
 
     fn open(&self, path: &Path) -> impl Future<Output = Result<Self::File, Error>> {
         self.open_options(path, OpenOptions::default())
@@ -47,14 +23,14 @@ pub trait Fs {
         &self,
         path: &Path,
         options: OpenOptions,
-    ) -> impl Future<Output = Result<Self::File, Error>>;
+    ) -> impl Future<Output = Result<Self::File, Error>> + MaybeSend;
 
-    fn create_dir(path: &Path) -> impl Future<Output = Result<(), Error>>;
+    fn create_dir(path: &Path) -> impl Future<Output = Result<(), Error>> + MaybeSend;
 
     fn list(
         &self,
         path: &Path,
-    ) -> impl Future<Output = Result<impl Stream<Item = Result<FileMeta, Error>>, Error>>;
+    ) -> impl Future<Output = Result<impl Stream<Item = Result<FileMeta, Error>>, Error>> + MaybeSend;
 
-    fn remove(&self, path: &Path) -> impl Future<Output = Result<(), Error>>;
+    fn remove(&self, path: &Path) -> impl Future<Output = Result<(), Error>> + MaybeSend;
 }
