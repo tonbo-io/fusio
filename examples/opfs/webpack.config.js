2276d645dc4584c16e1b9757199a654a7a5be891
crwen <1543720935@qq.com>
2024-11-04 15:17:22 +0800

    remove useless code

--- examples/opfs/webpack.config.js
+++ examples/opfs/webpack.config.js
@@ -3,25 +3,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
-module: {
-      rules: [
-        {
-         // You can provide better regexp
-          test: /\.wasm$/,
-          type: "asset/resource"
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "babel-loader",
-            }
-          ]
-        }
-      ]
-    }
-
 module.exports = {
   entry: "./index.js",
   output: {
